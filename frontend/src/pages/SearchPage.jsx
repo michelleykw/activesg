9dda30248edb784e7d8bcb660322292f4830e862
michelleykw <michelleykw99@gmail.com>
2022-03-23 00:36:26 +0800

    Changed Search Page to Dialog

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { makeStyles } from '@mui/styles';
 import Grid from '@mui/material/Grid';
+import Dialog from '@mui/material/Dialog';
 import AppButton from '../components/AppButton';
 import CategoryOptionsDialog from '../components/CategoryOptionsDialog';
 import RecentSearch from '../components/RecentSearch';
@@ -13,6 +14,9 @@ const useStyles = makeStyles(theme => ({
     },
     mt1: {
         marginTop: `${theme.spacing(1)} !important`
+    },
+    mt2: {
+        marginTop: `${theme.spacing(2)} !important`
     },
     mx2: {
         marginLeft: `${theme.spacing(2)} !important`,
@@ -27,7 +31,9 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
-function SearchPage({ isOldVersion, isSearching, recentSearchList, startSearch, resetSearchInput, cancelSearch, doSearch, removeRecentSearch, hasSearchValues, updateHasSearchValues, searchOptions, updateSearchOptions }) {
+// Perhaps can change this to a dialog (fullscreen) instead
+
+function SearchPage({ openPage, handleClosePage, isOldVersion, recentSearchList, startSearch, resetSearchInput, cancelSearch, doSearch, removeRecentSearch, hasSearchValues, updateHasSearchValues, searchOptions, updateSearchOptions }) {
     const classes = useStyles();
     const searchInput = useRef(null);
 
@@ -35,7 +41,7 @@ function SearchPage({ isOldVersion, isSearching, recentSearchList, startSearch, 
     const [searchCategory, setSearchCategory] = useState();
 
     useEffect(() => {
-    }, [isSearching, recentSearchList, hasSearchValues, searchOptions, openCategoryOptions, searchCategory]);
+    }, [recentSearchList, hasSearchValues, searchOptions, openCategoryOptions, searchCategory]);
 
     const doRecentSearch = item => {
         doSearch(item);
@@ -52,51 +58,85 @@ function SearchPage({ isOldVersion, isSearching, recentSearchList, startSearch, 
         setSearchCategory(null);
     };
 
-    return (
-        <Grid container alignItems="flex-start" justifyContent="flex-start">
-            {isOldVersion && (
-                <SearchBar 
-                    isSearching={isSearching} 
-                    startSearch={startSearch} 
-                    resetSearchInput={resetSearchInput}
-                    cancelSearch={cancelSearch}
-                    doSearch={doSearch}
-                    searchInput={searchInput}
-                    hasSearchValues={hasSearchValues}
-                    updateHasSearchValues={updateHasSearchValues}
-                    updateSearchOptions={updateSearchOptions}
+    const renderSearchBar = () => {
+        return (
+            <SearchBar 
+                isSearching
+                startSearch={startSearch} 
+                resetSearchInput={resetSearchInput}
+                cancelSearch={cancelSearch}
+                doSearch={doSearch}
+                searchInput={searchInput}
+                hasSearchValues={hasSearchValues}
+                updateHasSearchValues={updateHasSearchValues}
+                updateSearchOptions={updateSearchOptions}
+            />
+        );
+    };
+    
+    const renderCategoryButtons = () => {
+        return (
+            <Grid container item className={`${classes.mt1} ${classes.mx2}`}>
+                <AppButton 
+                    content="Sports" 
+                    className={`${classes.px2} ${classes.mr1}`} 
+                    onClick={() => openCategoryOptionsDialog('Sport')}
                 />
-            )}
-            {!hasSearchValues && isOldVersion && isSearching && (
-                <Grid container item className={`${classes.mt1} ${classes.mx2}`}>
-                    <AppButton 
-                        content="Sports" 
-                        className={`${classes.px2} ${classes.mr1}`} 
-                        onClick={() => openCategoryOptionsDialog('Sport')}
-                    />
-                    <AppButton 
-                        content="Venue" 
-                        onClick={() => openCategoryOptionsDialog('Venue')}
-                        className={`${classes.px2} ${classes.mr1}`} 
-                    />
-                </Grid>
-            )}
-            {!hasSearchValues && isSearching && recentSearchList && recentSearchList.length > 0 && (
-                <Grid container item className={classes.mt1}>
-                    <RecentSearch 
-                        recentSearchList={recentSearchList} 
-                        removeRecentSearch={removeRecentSearch}
-                        doRecentSearch={doRecentSearch}
-                    />
-                </Grid>
-            )}
-            {hasSearchValues && isSearching && (
-                <Grid container item className={classes.searchContainer}>
-                    <SearchList list={searchOptions} search={doRecentSearch} /> 
-                </Grid>
-            )}
-            <CategoryOptionsDialog doSearch={doRecentSearch} category={searchCategory} open={openCategoryOptions} fullScreen handleClose={closeCategoryOptionsDialog} />
-        </Grid>
+                <AppButton 
+                    content="Venue" 
+                    onClick={() => openCategoryOptionsDialog('Venue')}
+                    className={`${classes.px2} ${classes.mr1}`} 
+                />
+            </Grid>
+        );
+    };
+
+    const renderRecentSearch = () => {
+        return (
+            <Grid container item className={classes.mt1}>
+                <RecentSearch 
+                    recentSearchList={recentSearchList} 
+                    removeRecentSearch={removeRecentSearch}
+                    doRecentSearch={doRecentSearch}
+                />
+            </Grid>
+        );
+    };
+
+    const renderSearchList = () => {
+        return (
+            <Grid container item className={classes.searchContainer}>
+                <SearchList list={searchOptions} search={doRecentSearch} /> 
+            </Grid>
+        );
+    };
+
+    const renderCategoryDialog = () => {
+        return (
+            <CategoryOptionsDialog 
+                doSearch={doRecentSearch} 
+                category={searchCategory} 
+                open={openCategoryOptions} 
+                fullScreen 
+                handleClose={closeCategoryOptionsDialog} 
+            />
+        );
+    };
+
+    return (
+        <Dialog
+            fullScreen
+            open={openPage}
+            onClose={handleClosePage}
+        >
+            <Grid container alignItems="flex-start" justifyContent="flex-start" className={classes.mt2}>
+                {isOldVersion && renderSearchBar()}
+                {!hasSearchValues && isOldVersion && renderCategoryButtons()}
+                {!hasSearchValues && recentSearchList && recentSearchList.length > 0 && renderRecentSearch()}
+                {hasSearchValues && renderSearchList()}
+                {renderCategoryDialog()}
+            </Grid>
+        </Dialog>
     );
 }
 
